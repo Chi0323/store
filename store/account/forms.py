933a2ad3a17907aab68b33c67646350fb5e2ed47
Chi0323 <44564524+Chi0323@users.noreply.github.com>
2019-01-08 04:04:51 +0800

    Add files via upload

--- store/account/forms.py
+++ store/account/forms.py
@@ -1,19 +1,78 @@
 from django import forms
 from account.models import User
 
-
 class UserForm(forms.ModelForm):
-    username = forms.CharField(label='帳號')
-    password = forms.CharField(label='密碼', widget=forms.PasswordInput())
-    password2 = forms.CharField(label='確認密碼', widget=forms.PasswordInput())
-    fullName = forms.CharField(label='姓名', max_length=128)
-    mobilephone = forms.CharField(label='行動電話', max_length=10)
-    birthday = forms.DateField(label='生日',widget=forms.TextInput(attrs={'type':'date'}) )    
-    address = forms.CharField(label='住址', max_length=128)
+    username = forms.CharField(
+        required=True,
+        error_messages={
+            "required": "用户名不能為空",
+            "min_length": "用户名長度不能小於6", 
+            "max_length": "用户名長度不能小於15"
+        },
+        label='', 
+        widget=forms.TextInput(attrs={"placeholder": "帳號", "class": "form-control"})
+        )
+    password = forms.CharField(
+        required=True,
+        min_length=6,
+        max_length=30,
+        error_messages={
+            "required": "密碼不能為空",
+            "min_length": "密碼長度不能小於6",
+            "max_length": "用户名長度不能超過30",
+        },
+        label='', 
+        widget=forms.PasswordInput(attrs={"placeholder": "請輸入密碼", "class": "form-control"})
+        )
+    password2 = forms.CharField(
+        required=True,
+        min_length=6,
+        max_length=30,
+        error_messages={
+            "required": "密碼不能為空",
+            "min_length": "密碼長度不能小於6",
+            "max_length": "用户名長度不能超過30",
+        },
+        label='', 
+        widget=forms.PasswordInput(attrs={"placeholder": "再次輸入密碼", "class": "form-control"})
+        )
+    fullName = forms.CharField(
+        required=True,
+        error_messages={
+            "required": "姓名不能為空",
+        },
+        label='', 
+        widget=forms.TextInput(attrs={"placeholder": "姓名", "class": "form-control"})
+        )
+    telephone = forms.CharField(
+        required=True,
+        max_length=11,
+        min_length=11,
+        error_messages={
+            "required": "手機號碼不能為空",
+            "min_length": "手機號碼長度必須是11位",
+            "max_length": "手機號碼長度必須是11位",
+        },
+        label='', 
+        widget=forms.TextInput(attrs={"placeholder": "電話", "class": "form-control"})
+        )
+    birthday = forms.DateField(
+        label='', 
+        widget=forms.TextInput(attrs={"placeholder": "生日", 'type':'date',"class": "form-control"})
+        )   
+    address = forms.CharField(
+        required=True,
+        max_length=128,
+        error_messages={
+            "required": "地址不能為空",
+        },
+        label='', 
+        widget=forms.TextInput(attrs={"placeholder": "地址", "class": "form-control"})
+        )
     
     class Meta:
         model = User
-        fields = ['username', 'password', 'password2', 'fullName', 'mobilephone','birthday', 'address']
+        fields = ['username', 'password', 'password2', 'fullName', 'telephone','birthday', 'address']
 
     def clean_password2(self):
         password = self.cleaned_data.get('password')
