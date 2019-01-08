7091d84ea9ec1252a3b5e5f5f22080198ab0286d
Chi0323 <zx0918884233@gmail.com>
2019-01-09 02:55:01 +0800

    1/9

--- store/product/forms.py
+++ store/product/forms.py
@@ -6,6 +6,7 @@ class ProductForm(forms.ModelForm):
     title = forms.CharField(label='標題', max_length=128)
     content = forms.CharField(label='內容', widget=forms.Textarea)
     specifications = forms.CharField(label='規格', max_length=100)
+    brand = forms.CharField(label='品牌', max_length=100)
     price = forms.IntegerField(label='價格', widget=forms.NumberInput())
     quantity = forms.IntegerField(label='數量', widget=forms.NumberInput())
     
--- store/product/models.py
+++ store/product/models.py
@@ -4,6 +4,7 @@ class Product(models.Model):
     title = models.CharField(max_length=128)
     content = models.TextField()
     specifications = models.CharField(max_length=128)
+    brand = models.CharField(max_length=128)
     price = models.IntegerField()
     quantity = models.IntegerField()                   
     
