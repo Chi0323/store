fd57e8aa5de7443755043f712a1bd2b121534032
Chi0323 <44564524+Chi0323@users.noreply.github.com>
2019-01-13 16:51:42 +0800

    Add files via upload

--- store/account/models.py
+++ store/account/models.py
@@ -3,12 +3,12 @@ from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
-    username = models.EmailField(max_length=50, unique=True)
+    username = models.CharField(max_length=50, unique=True)
     fullName = models.CharField(max_length=128,null=True)
     telephone = models.CharField(max_length=10,null=True)
     birthday = models.DateField(null=True)
     address = models.CharField(max_length=128,null=True)
-    
+      
     ASKTYPE=((1,'訂單'),(2,'出貨'),(3,'退貨/退款'),(4,'取消訂單'),(5,'其他'))
     asktype=models.CharField(max_length=1,choices=ASKTYPE,null=True)
     asktittle = models.CharField(max_length=128,null=True)
