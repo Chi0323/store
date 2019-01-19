81dc0977f3770c3bd1cefef59aaeef862c112078
user <user@DESKTOP-U4G8GBM>
2019-01-20 02:41:03 +0800

    1/20

--- store/cart/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from cart import views
-
-app_name = 'cart'
-urlpatterns = [
-    path('', views.cart, name='cart'),
-]
--- store/cart/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-def cart(request):
-    '''
-    Render the cart page
-    '''
-    return render(request, 'cart/cart.html')
-
